e3996f200352b0467e9875cac56489aa7e13f814
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-06-13 01:53:40 +0400

    implement agents table with page _ trpc uery server fetch

---
+++ src/modules/agents/server/procedures.ts
@@ -0,0 +1,20 @@
+import { db } from "@/db";
+import { agents } from "@/db/schema";
+import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+// import { TRPCError } from "@trpc/server";
+
+export const agentsRouter = createTRPCRouter({
+  getMany: baseProcedure.query(async () => {
+    const data = await db.select().from(agents);
+
+    return data;
+  }),
+});
+
+// Uncomment to test error handling
+
+// await new Promise((resolve) => setTimeout(resolve, 5000));
+// throw new TRPCError({
+//   code: "INTERNAL_SERVER_ERROR",
+//   message: "Error fetching agents",
+// });
