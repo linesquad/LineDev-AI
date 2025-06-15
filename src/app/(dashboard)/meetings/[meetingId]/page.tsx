32c87b331cc0854ce9eb3f8a4767676ab194defc
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-06-15 22:22:45 +0400

    implement meeting page and trpc procedures

---
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -0,0 +1,3 @@
+export default function MeetingPage() {
+  return <div>Meeting</div>;
+}
---
+++ src/app/(dashboard)/meetings/page.tsx
@@ -0,0 +1,23 @@
+import {
+  MeetingsView,
+  MeetingsViewError,
+  MeetingsViewLoading,
+} from "@/modules/meetings/views/meetings-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+
+export default function MeetingsPage() {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}));
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<MeetingsViewLoading />}>
+        <ErrorBoundary fallback={<MeetingsViewError />}>
+          <MeetingsView />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
+}
